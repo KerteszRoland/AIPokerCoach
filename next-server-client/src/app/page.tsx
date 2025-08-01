5dea9f86e71049bd0d12ed62530005237649e57c
Roland Kertész <roland.kertesz@itsquad.hu>
2025-08-01 21:30:24 +0200

    fix build errors

--- next-server-client/src/app/page.tsx
+++ next-server-client/src/app/page.tsx
@@ -1,12 +1,8 @@
-import Card from "../components/server/Card";
-import { FaUser } from "react-icons/fa";
-import LiveRangeChart from "../components/client/LiveRangeChart";
 import PreviousRoundsCard from "../components/client/PreviousRoundsCard";
 import SignedInServer from "@/components/server/SignedInServer";
 import SignedOutServer from "@/components/server/SignedOutServer";
 import getSessionOrRedirect from "@/server/getSessionOrRedirect";
 import { getHands } from "@/server/serverRequests/hand";
-import CoachReviewCard from "@/components/server/CoachReviewCard";
 
 export default async function Home() {
   const session = await getSessionOrRedirect();
@@ -20,7 +16,6 @@ export default async function Home() {
       <SignedInServer>
         <div className="flex items-start justify-between max-w-[1400px] w-full px-8 pt-8">
           <div className="flex flex-col max-w-[400px] gap-4">
-            <CoachReviewCard />
             <PreviousRoundsCard hands={hands} />
           </div>
         </div>
